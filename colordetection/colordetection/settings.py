6d208693f2be72d4bcee40f381e037261812be05
Harmeet Singh <harmeetchahal9891@gmail.com>
2023-06-30 01:42:51 +0530

    api changes

--- colordetection/colordetection/settings.py
+++ colordetection/colordetection/settings.py
@@ -1,6 +1,7 @@
-
-
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,13 +68,16 @@ WSGI_APPLICATION = 'colordetection.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://color_detection_9xpk_user:rfbWPWsOTzbDCHWEoAvRKO0B0nooalBA@dpg-cieua9p5rnujc4pf8c2g-a.singapore-postgres.render.com/color_detection_9xpk')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -114,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
